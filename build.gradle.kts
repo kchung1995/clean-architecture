856d712e7310f1cea91850ecec495e8e80bab8a4
Kuyho Chung <kchung1995@gmail.com>
2024-09-18 08:58:13 +0900

    feat: Kotlin 버전업 및 jlleitschuh-ktlint 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
+    kotlin("jvm") version "2.0.20"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "com.katfun"
@@ -34,4 +35,17 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+/**
+ * Disabling ktlint rules as package-name does not allow backticks
+ * see https://pinterest.github.io/ktlint/0.49.1/faq/#how-do-i-enable-or-disable-a-rule
+ * see https://github.com/JLLeitschuh/ktlint-gradle?tab=readme-ov-file#configuration
+ */
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    additionalEditorconfig.set(
+        mapOf(
+            "ktlint_standard_package-name" to "disabled",
+        ),
+    )
 }
